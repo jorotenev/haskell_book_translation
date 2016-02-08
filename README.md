729748fd75772a56d7146e1b82794c2b9c247442
Georgi Tenev <chipsanbg@gmail.com>
2016-02-09 01:04:34 +0200

    started working on the intro

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Bulgarian translation of the learnyouahaskell.com Haskell book, written by Miran
 http://haskellbg.herokuapp.com/
 
 
-Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)  
 http://creativecommons.org/licenses/by-nc-sa/3.0/
--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, send_from_directory
 app = Flask(__name__,static_url_path='/static',template_folder=".")
 
-
+tutorial_name = "asd"
 @app.route("/")
 def a():
-    return render_template('introduction')
+    return render_template('introduction', tut_name = tutorial_name)
 
 @app.route("/<file>")
 def index(file):
-     return render_template(file)
+     return render_template(file, tut_name = tutorial_name)
 
-@app.route("/<path>")
+@app.route("/static/<path>")
 def boo(path):
     return send_from_directory('static', path)
 
